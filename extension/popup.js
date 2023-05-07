520220abfbc709b2532fe936d7a3a7ecc3d93179
hg <hg@hgs-MacBook-Air.lan>
2023-05-07 12:37:18 -0400

    close window

--- extension/popup.js
+++ extension/popup.js
@@ -3,9 +3,7 @@ document.addEventListener('DOMContentLoaded', documentEvents  , false);
 function myAction(input) { 
     console.log("input value is : " + input.value);
     chrome.storage.local.set({"audience": input.value});
-    // do processing with data
-    // you need to right click the extension icon and choose "inspect popup"
-    // to view the messages appearing on the console.
+    window.close();
 }
 
 function documentEvents() {    
