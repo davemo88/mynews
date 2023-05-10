941a3a6c9b343e916e3c5a1d3d452f8acb4ad304
hg <hg@hgs-MacBook-Air.lan>
2023-05-10 01:37:35 -0400

    whole article on fox

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -4,24 +4,39 @@
       await rewriteHeadline();
     }
   );
-  await rewriteHeadline();
+  const a = await audience();
+  await rewriteHeadline(a);
+  await rewriteArticle(a);
 })();
 
-async function rewriteHeadline() {
-  const a = (await chrome.storage.local.get("audience")).audience ?? "Medieval Peasant";
+async function rewriteHeadline(audience) {
   let hl = headline();
-  hl.textContent = await getAudienceHeadline(hl, a);
+  hl.textContent = await newHeadline(hl, audience);
+}
+
+async function rewriteArticle(audience) {
+  await Promise.all([...article()].map(async chunk => {
+    console.log(chunk);
+    chunk.textContent = await newHeadline(chunk, audience);
+  }));
 }
 
 const headline = () => document.querySelector(".headline");
 
-async function getAudienceHeadline(headline, audience) {
+const article = () => document.querySelectorAll(".story-text__paragraph");
+
+async function newHeadline(headline, audience) {
   const response = await fetch(
     "http://localhost:3000/mynews",
     request(headline, audience)
   );
   const body = await response.json();
   return unquote(body);
+}
+
+async function audience() {
+  const a = (await chrome.storage.local.get("audience")).audience ?? "Medieval Peasant";
+  return a;
 }
 
 const request = (headline, audience) => ({
